f1c95e66238c875b03e9bfec1a96e3121d9de218
Daniel STAN <daniel.stan@crans.org>
2014-06-23 19:08:33 +0200

    client: vérification du certificat

--- client/ask_code.py
+++ client/ask_code.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-
 import requests
 import json
-import config
 import sys
+import os
+from . import config
 
 BASE_URL = 'http://localhost:8000/rpc'
+
+CA = os.path.join(os.path.dirname(__file__), '../ssl/cacert.org_chain.pem')
 
 def ask_code(code):
     req = requests.post('%s/code' % BASE_URL, {
       'id': config.pk_local,
       'shared_secret': config.shared_secret,
       'code': code,
-     })
+      },
+      verify=CA,  
+     )
 
     if req.status_code != 200:
         raise Exception("Return code %d" % req.status_code)
